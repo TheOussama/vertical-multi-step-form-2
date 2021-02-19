b035b86a8f8690d71be0fec14bca6f63897955c4
oussama sikaddour <34127545+TheOussama@users.noreply.github.com>
2021-02-19 21:39:13 +0100

    vertical-multi-step-fomr awesome

--- README.md
+++
@@ -1,2 +0,0 @@
-# vertical-multi-step-form-2
- an awesome looking multi-step form with a vertical progress bar, in HTML, CSS, and vanilla javascript, if you like it don't forget to like share and sub 
--- main.js
+++ main.js
@@ -1,26 +1,7 @@
-let form1 = document.querySelector(".form-1");
-let form2 = document.querySelector(".form-2");
-let form3 = document.querySelector(".form-3");
-let next1 = document.querySelector("#next1");
-let next2 = document.querySelector("#next2");
-let back1 = document.querySelector("#back1");
-let back2 = document.querySelector("#back2");
-
-let progress = document.querySelector(".form__progress");
+const form1 = document.querySelector(".form-1");
+const forms = document.querySelectorAll(".form");
+const progress = document.querySelector(".form__progress");
 const inputs = document.querySelectorAll("input");
-
-slide(progress, next1, form1, form2, "-42rem", "10rem", "66.666667%");
-slide(progress, back1, form1, form2, "10rem", "42rem", "33.333337%");
-slide(progress, next2, form2, form3, "-42rem", "10rem", "99.999997%");
-slide(progress, back2, form2, form3, "10rem", "42rem", "66.666667%");
-
-function slide(progress, link, form1, form2, leftform1, leftform2, height) {
-  link.addEventListener("click", () => {
-    form1.style.left = leftform1;
-    form2.style.left = leftform2;
-    progress.style.height = height;
-  });
-}
 
 function focusFunc() {
   let parent = this.parentNode;
@@ -37,4 +18,63 @@ function blurFunc() {
 inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
+});
+
+forms.forEach((form, index) => {
+  const height = 100 / forms.length;
+  form.classList.add("next");
+  const buttons = form.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const action = button.textContent.trim();
+      if (index < 0) {
+        index = 0;
+      }
+      if (index > buttons.length) {
+        index = buttons.length - 1;
+      }
+      if (action === "Next") {
+        forms[index].classList.add("previous");
+        forms[index].classList.remove("active");
+        forms[index + 1].classList.add("active");
+        forms[index + 1].classList.remove("next");
+        progress.style.height = `${height + height * (index + 1)}%`;
+
+        document
+          .querySelector(`.form__step-${index + 1}`)
+          .classList.add("active");
+      }
+      if (action === "Back") {
+        forms[index - 1].classList.add("active");
+        forms[index - 1].classList.remove("previous");
+        forms[index].classList.add("next");
+        forms[index].classList.remove("active");
+        progress.style.height = `${height * index - forms.length}%`;
+
+        document
+          .querySelector(`.form__step-${index}`)
+          .classList.remove("active");
+      }
+    });
+  });
+});
+form1.classList.remove("next");
+form1.classList.add("active");
+
+const iconLock = document.querySelector("#pwdIcon");
+const pwd = document.querySelector("#pwd");
+function show(iconLock, input) {
+  if (input.type === "password") {
+    input.type = "text";
+    iconLock.classList.remove("fa-lock");
+    iconLock.classList.add("fa-lock-open");
+  } else {
+    input.type = "password";
+    iconLock.classList.add("fa-lock");
+    iconLock.classList.remove("fa-lock-open");
+  }
+}
+
+iconLock.addEventListener("click", () => {
+  show(iconLock, pwd);
 });
